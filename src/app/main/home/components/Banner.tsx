f5b87415fb1e3ff879ede4a3ac384c9ca74fd934
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2024-12-31 01:03:34 +0700

    feat(UI): fix Banner UI

--- src/app/main/home/components/Banner.tsx
+++ src/app/main/home/components/Banner.tsx
@@ -4,7 +4,7 @@ import AvatarSection from "./sections/AvatarSection";
 const Banner = () => {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden">
-      <div className=" relative z-10 flex items-center justify-between w-full px-6 py-10 mt-0 sm:mt-24 md:flex-row flex-col-reverse md:space-x-8">
+      <div className=" relative z-10 flex items-center justify-between w-full px-6 md:flex-row flex-col-reverse md:space-x-8">
         <IntroSection />
         <AvatarSection />
       </div>
