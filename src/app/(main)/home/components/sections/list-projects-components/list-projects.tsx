4ae6b15ca8aa194605a97b1b8601b937641628f9
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-03-02 11:23:53 +0700

    feat(component): fix grid list project

--- src/app/(main)/home/components/sections/list-projects-components/list-projects.tsx
+++ src/app/(main)/home/components/sections/list-projects-components/list-projects.tsx
@@ -45,7 +45,7 @@ const ListProjects = () => {
     );
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-8">
             {projects.map((project: Project, index) => (
                 <motion.div
                     className="relative"
