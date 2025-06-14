2a6cc4c1f58b4983623511bb972386d3e4c01859
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-06-14 23:20:25 +0700

    feat: fix home

--- src/app/(main)/home/components/banner/banner.tsx
+++ src/app/(main)/home/components/banner/banner.tsx
@@ -3,7 +3,7 @@ import Avatar from "./avatar";
 
 const Banner = () => {
   return (
-    <div className="flex md:pt-12 w-full flex-col items-center justify-center">
+    <div className="flex md:pt-20 w-full flex-col items-center justify-center">
       <div className="z-10 relative flex items-center justify-center w-full max-w-5xl md:flex-row flex-col-reverse md:space-x-8">
         <Intro />
         <Avatar />
